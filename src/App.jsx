832517fbac301e94bfb82b0ce54b269d64d99271
MalaviyaIsha <isha.malaviya@exhibytesolution.com>
2024-07-11 19:10:30 +0530

    push code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-[50px]">Hello, React with Parcel.js</h1>
+    </div>
+  );
+}
+
+export default App;
